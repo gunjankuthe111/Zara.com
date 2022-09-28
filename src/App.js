78d7077c6424bbe46406e9a5847756af3c5aef24
Gunjan Kuthe <gkuthe1997@gmail.com>
2022-09-29 00:08:29 +0530

    added modifined day-2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 // import Mens from './pages/Mens';
 import Footer from "./Components/Footer"
+import Kids from './pages/Kids';
 import Womens from './pages/Womens';
 
 function App() {
   return (
     <div className="App">
       {/* <Mens/> */}
-      <Womens/>
+      {/* <Womens/> */}
+      <Kids/>
       <Footer/>
     </div>
   );
